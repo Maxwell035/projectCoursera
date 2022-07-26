fd7520ffdbbb335ea2b63e6ca702e8d89b807087
Lord Kelvin <oscarbenitez198529@gmail.com>
2022-07-26 16:38:16 -0500

    versión distribuible

---
+++ functionsBoots.js
@@ -0,0 +1,41 @@
+const eventModal1= document.getElementById('contactModal-1')
+
+eventModal1.addEventListener("show.bs.modal", e=>{
+    document.getElementById('contactBtn-1').setAttribute('disabled', true);
+})
+
+const eventModal2= document.getElementById('contactModal-2')
+
+eventModal2.addEventListener("show.bs.modal", e=>{
+    document.getElementById('contactBtn-2').setAttribute('disabled', true);
+})
+
+const eventModal3= document.getElementById('contactModal-3')
+
+eventModal3.addEventListener("show.bs.modal", e=>{
+    document.getElementById('contactBtn-3').setAttribute('disabled', true);
+})
+
+const eventModal4= document.getElementById('contactModal-4')
+
+eventModal4.addEventListener("show.bs.modal", e=>{
+    document.getElementById('contactBtn-4').setAttribute('disabled', true);
+})
+
+const eventModal5= document.getElementById('contactModal-5')
+
+eventModal5.addEventListener("show.bs.modal", e=>{
+    document.getElementById('contactBtn-5').setAttribute('disabled', true);
+})
+
+const eventModal6= document.getElementById('contactModal-6')
+
+eventModal6.addEventListener("show.bs.modal", e=>{
+    document.getElementById('contactBtn-6').setAttribute('disabled', true);
+})
+
+const eventModal7= document.getElementById('contactModal-7')
+
+eventModal7.addEventListener("show.bs.modal", e=>{
+    document.getElementById('contactBtn-7').setAttribute('disabled', true);
+})
